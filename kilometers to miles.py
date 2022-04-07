9f42a0f201f72a73089c9d641b21ce57980dfd38
Makhon Chandra Barman <103095870+MakhonChandra@users.noreply.github.com>
2022-04-07 12:42:45 -0700

    Add files via upload

---
+++ kilometers to miles.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 12:25:35 2022
+
+@author: Makhon Chandra
+"""
+
+kilometres = int(input(" Enter the Number of Kilometres: "))
+
+miles = kilometres / 1.609
+
+print( kilometres, " Km converts to", miles, "miles.")
